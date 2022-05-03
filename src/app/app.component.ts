c469b53d8aa98d2f4533e565d684aa7e520e7141
Wilian Souza <wilian.souza@dtidigital.com.br>
2022-05-03 12:24:45 -0300

    Criado primeiro component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-  descripton = 'papagaio';
-  url = 'https://www.cevek.com.br/blog/wp-content/uploads/2020/06/papagaio-de-estima%C3%A7%C3%A3o-e-suas-principais-caracter%C3%ADsticas-min-1140x641.jpg'
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { PhotoComponent } from './photo/photo.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -5,7 +6,8 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoComponent
   ],
   imports: [
     BrowserModule
