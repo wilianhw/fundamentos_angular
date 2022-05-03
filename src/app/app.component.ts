5107d77a1aedc459c9b5525aa5085c34f837ee2f
Wilian Souza <wilian.souza@dtidigital.com.br>
2022-05-03 11:23:02 -0300

    Aula sobre data binding - Interpolação e Property Bindind.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  descripton = 'papagaio';
+  url = 'https://www.cevek.com.br/blog/wp-content/uploads/2020/06/papagaio-de-estima%C3%A7%C3%A3o-e-suas-principais-caracter%C3%ADsticas-min-1140x641.jpg'
 }
